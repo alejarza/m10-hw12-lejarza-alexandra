042dfd0063f61a348a523764fe857546ce7aeed4
Alexandra <alexandralejarza@gmail.com>
2021-11-28 17:33:27 -0500

    Edit js file to complete tests

--- index.js
+++ index.js
@@ -1,2 +1,26 @@
 // YOUR CODE HERE
-//create a class Car
+//Create a class Car
+class Car {
+    constructor(make, model, year) {
+        this.make = make
+        this.model = model
+        this.year = year
+    }
+    
+    honk() {
+        console.log('BEEP BEEP!')
+    }
+    performMaintenance() {
+        //Log to the console after 3 seconds
+        var timerId = setTimeout(function() {
+        console.log('maintenance complete')
+        }, 3000)
+        
+    }
+}
+
+//Create variable 
+var mySweetRide = new Car('pontiac','fiero',1988)
+
+mySweetRide.honk()
+mySweetRide.performMaintenance()
